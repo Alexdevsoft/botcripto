0da4633d08e950be00fb546998621bc810b42a7a
Alexdevsoft <alexhavilla2022@gmail.com>
2025-02-12 13:26:12 -0300

    Código completo

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const axios = require("axios")
+const axios = require("axios");
+const crypto = require("crypto");
 
 const SYMBOL = "BTCUSDT";
+const QUANTITY = "0.0001";
 const PERIOD = 14;
 
-const API_URL = "https://api.binance.com"; //https://testnet.binance.vision
+const API_URL = "https://testnet.binance.vision"; //https://api.binance.com
+const API_KEY = "6YEyQZhJlUGmdleEnl7jtV4N8c5jblh80TQb56VBAPgtx1IRqSzmfn4GRpnY6QUF"
+const SECRET_KEY = "dO5oq43vnae594d1jyCA8IiDCBxWfZTRogrvlKR6Obi2IU0XLBZ73TMiNlDtaXZz"
 
 function averages(prices, period, startIndex) {
     let gains = 0, losses = 0;
@@ -42,6 +46,37 @@ function RSI(prices, period) {
     return 100 - (100 / (1 + rs));
 }
 
+async function newOrder(symbol, quantity, side) {
+    const order = { symbol, quantity, side };
+    order.type = "MARKET";
+    const serverTime = await fetch('https://api.binance.com/api/v3/time').then(response => response.json());
+    const timeDifference = serverTime.serverTime - Date.now();
+    order.timestamp = Date.now() + timeDifference;
+
+
+
+
+    const signature = crypto.Hmac("sha256", SECRET_KEY)
+        .update(new URLSearchParams(order).toString())
+        .digest("hex");
+
+    order.signature = signature;
+
+    try {
+        const { data } = await axios.post(API_URL + "/api/v3/order",
+            new URLSearchParams(order).toString(),
+            {
+                headers: { "X-MBX-APIKEY": API_KEY }
+            }
+        )
+
+        console.log(data);
+
+    } catch (err) {
+        console.error(err.response.data);
+    }
+}
+
 let isOpened = false;
 
 async function start() {
@@ -56,13 +91,17 @@ async function start() {
     const prices = data.map(k => parseFloat(k[4]));
     const rsi = RSI(prices, PERIOD);
     console.log("RSI: " + rsi);
+    console.log("Já comprei? ", isOpened);
 
     if (rsi < 30 && isOpened === false) {
         console.log("sobrevendido, hora de comprar.");
         isOpened = true;
+        newOrder(SYMBOL, QUANTITY, "BUY");
+        
     }
     else if (rsi > 70 && isOpened === true) {
         console.log("sobrecomprado, hora de vender.");
+        newOrder(SYMBOL, QUANTITY, "SELL");
         isOpened = false;
     }
     else
